6eb85c733fa24bda8d30a3b82ced42e7d9764d78
Huclark <vhuclark@gmail.com>
2023-10-30 15:10:34 +0000

    task 102 correction

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    const_number = 98  # LOAD_CONST 1 (98)
-    const_number = const_number ** a  # LOAD_FAST 0 (a) | BINARY_POWER
-    const_number = const_number + b  # LOAD_FAST 1 (b) | BINARY_ADD
-    return const_number  # RETURN_VALUE
+    return ((a ** b) + 98)
