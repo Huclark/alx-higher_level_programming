f4a6c327d391dfba64e9e91e334e962538530037
Huclark <vhuclark@gmail.com>
2023-10-30 14:42:37 +0000

    task 10 correction

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -16,6 +16,9 @@ typedef struct listint_t
 } listint_t;
 
 /* Custom prototypes */
-int check_Cycle(listint_t *list);
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
 
 #endif
