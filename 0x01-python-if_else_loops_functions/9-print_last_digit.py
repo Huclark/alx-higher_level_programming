38c5b0965556fbb69c82e5ffd2c2c84683c0bc8b
Huclark <vhuclark@gmail.com>
2023-10-31 16:44:36 +0000

    pycodestyle correction for task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,5 +2,5 @@
 
 # print_last_digit - prints the last digit of a number
 def print_last_digit(number):
-    print("{}".format(abs(number) % 10), end="") # print last digit
-    return (abs(number) % 10) # return last digit
+    print("{}".format(abs(number) % 10), end="")  # print last digit
+    return (abs(number) % 10)  # return last digit
