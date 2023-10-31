3bbac7ee7b7441a69c1cf1b6657fc82f5a484aea
Huclark <vhuclark@gmail.com>
2023-10-31 20:16:24 +0000

    task 14 pycodestyle correction

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 # prints the ASCII alphabet, in reverse order, alternating lowercase
-# and uppercase (z in lowercase and Y in uppercase) , not followed by a new line
+# and uppercase (z in lowercase and Y in uppercase), not followed by a new line
 
 for i in range(ord('z'), ord('a') - 1, -1):
     if i % 2 == 0:
