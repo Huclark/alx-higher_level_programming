5753175632a511ab3da33a82150cb69f265a07b3
Huclark <vhuclark@gmail.com>
2023-10-31 20:14:00 +0000

    task 14

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# prints the ASCII alphabet, in reverse order, alternating lowercase
+# and uppercase (z in lowercase and Y in uppercase) , not followed by a new line
+
+for i in range(ord('z'), ord('a') - 1, -1):
+    if i % 2 == 0:
+        print("{}".format(chr(i)), end="")
+    else:
+        print("{}".format(chr(i - 32)), end="")
