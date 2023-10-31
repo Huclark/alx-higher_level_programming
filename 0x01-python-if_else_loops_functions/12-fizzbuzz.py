6bcd711d78454ec7638cd3779a172a8ad6e103f1
Huclark <vhuclark@gmail.com>
2023-10-31 17:30:06 +0000

    pycodestyle corrected for task 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,10 +5,10 @@ def fizzbuzz():
     # set loop to iterate from 1 to 100
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 != 0:  # if multiple of 3 only
-            print("{}".format("Fizz"), end= " ")
+            print("{}".format("Fizz"), end=" ")
         elif i % 5 == 0 and i % 3 != 0:  # if multiple of 5 only
-            print("{}".format("Buzz"), end= " ")
+            print("{}".format("Buzz"), end=" ")
         elif i % 3 == 0 and i % 5 == 0:  # if multiple of both 3 and 5
-            print("{}".format("FizzBuzz"), end= " ")
+            print("{}".format("FizzBuzz"), end=" ")
         else:
             print("{:d}".format(i), end=" ")
