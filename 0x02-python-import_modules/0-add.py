dd5e016abf44c2e7d0fe958e21e3bbea545b2599
Huclark <vhuclark@gmail.com>
2023-11-02 14:17:59 +0000

    task 0 correction

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
+from add_0 import add
 
-    from add_0 import add
+a = 1
+b = 2
 
-    a = 1
-    b = 2
+a = int(a)
+b = int(b)
 
-    a = int(a)
-    b = int(b)
-
-    print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} + {} = {}".format(a, b, add(a, b)))
