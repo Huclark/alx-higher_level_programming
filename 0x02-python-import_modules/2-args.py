f8d232703a8ac0fbc78428159e2c264f8d6efccc
Huclark <vhuclark@gmail.com>
2023-11-02 15:32:40 +0000

    task 2 correction

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,11 +8,15 @@ if __name__ == "__main__":
 
     argc = len(sys.argv)
 
-    if argc == 2:
+    if argc == 1:
+        print("{} arguments.".formmat(argc - 1))
+
+    elif argc == 2:
         print("{} argument:".format(argc - 1))
+
     else:
-        print("{} arguments".format(argc - 1, "." if argc == 1 else ":"))
+        print("{} arguments:".format(argc - 1))
 
     if argc > 1:
         for i in range(1, argc):
-            print("{}: {}".format(i, sys.argv[i]))
+            print("{}: {}". format(i, sys.argv[i]))
