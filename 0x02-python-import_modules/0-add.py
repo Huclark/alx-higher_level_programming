0ae422d179be5c4c6d64ae25b4ef73087e648ca8
Huclark <vhuclark@gmail.com>
2023-11-02 14:15:33 +0000

    task 0 correction

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python3
 
-# This program imports the function def add(a, b): from the file add_0.py
-# and prints the result of the addition 1 + 2 = 3
-
 if __name__ == "__main__":
 
-    from add_0 import add  # import function
+    from add_0 import add
 
-    # assign values to variables
     a = 1
     b = 2
 
-    # typecast a and b
     a = int(a)
     b = int(b)
 
-    # print result using string format while passing add(a,b) as an
-    # argument to print the result
     print("{} + {} = {}".format(a, b, add(a, b)))
