748d79b6632dd483118cd24d45984699e3235ccf
Huclark <vhuclark@gmail.com>
2023-11-02 14:12:23 +0000

    task 0 correction

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,12 +4,17 @@
 and prints the result of the addition 1 + 2 = 3 '''
 
 if __name__ == "__main__":
+
     from add_0 import add  # import function
 
     # assign values to variables
     a = 1
     b = 2
 
+    # typecast a and b
+    a = int(a)
+    b = int(b)
+
     ''' print result using string format while passing add(a,b) as an
     argument to print the result '''
-    print("{} + {} = {}".format(int(a), int(b), add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
