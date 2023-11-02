f62f3d1b93e277245b2ffcb8007e753ca452cb0b
Huclark <vhuclark@gmail.com>
2023-11-02 14:03:17 +0000

    task 0 correction

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,6 +3,7 @@
 ''' This program imports the function def add(a, b): from the file add_0.py
 and prints the result of the addition 1 + 2 = 3 '''
 
+
 from add_0 import add  # import function
 
 # assign values to variables
@@ -11,5 +12,5 @@ b = 2
 
 ''' print result using string format while passing add(a,b) as an
 argument to print the result '''
-
-print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    print("{} + {} = {}".format(a, b, add(a, b)))
