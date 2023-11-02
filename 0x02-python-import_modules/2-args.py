e97062e808b3dfbc8204ab6371bf70d8d6c2e670
Huclark <vhuclark@gmail.com>
2023-11-02 15:36:45 +0000

    task 2 correction

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,17 +4,17 @@ if __name__ == "__main__":
     """Prints the number of and the list of its arguments"""
     import sys
 
-    argc = len(sys.argv)
+    argument_count = len(sys.argv)
 
-    if argc == 1:
-        print("{} arguments.".formmat(argc - 1))
+    if argument_count == 1:
+        print("{} arguments.".formmat(argument_count - 1))
 
-    elif argc == 2:
-        print("{} argument:".format(argc - 1))
+    elif argument_count == 2:
+        print("{} argument:".format(argument_count - 1))
 
     else:
-        print("{} arguments:".format(argc - 1))
+        print("{} arguments:".format(argument_count - 1))
 
-    if argc > 1:
-        for i in range(1, argc):
+    if argument_count > 1:
+        for i in range(1, argument_count):
             print("{}: {}". format(i, sys.argv[i]))
