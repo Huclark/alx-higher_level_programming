2a530897b84fd8b78f66679ba26ada36152b89c2
Huclark <vhuclark@gmail.com>
2023-11-02 21:17:58 +0000

    task 5

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Imports the variable a from the file variable_load_5.py and prints its value"""
+if __name__ == "__main__":
+    import variable_load_5
+
+    names_defined = dir(variable_load_5)
+
+    if "a" in names_defined:
+        a = getattr(variable_load_5, "a")
+        print (a)
