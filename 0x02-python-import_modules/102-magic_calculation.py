ed5ef27b16c6b4a051a42276a5a3f1d89726e648
Huclark <vhuclark@gmail.com>
2023-11-02 22:35:49 +0000

    task 102 pycodestyle correction

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -3,13 +3,16 @@
 """This function does exactly the same as the bytecode in
 the bytecode102 file
 """
+
+
 def magic_calculation(a, b):
+
     from magic_calculation_102 import add, sub
 
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
-        return (c)
+        return c
     else:
-        return(sub(a, b))
+        return sub(a, b)
