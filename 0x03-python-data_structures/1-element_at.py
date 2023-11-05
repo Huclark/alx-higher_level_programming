85ebb19184ad4f304193596ec69f6bd2d8e46dce
Huclark <vhuclark@gmail.com>
2023-11-05 13:43:09 +0000

    task 1 correction

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -8,5 +8,5 @@ element_at - Retrieves an element from a list like in C
 def element_at(my_list, idx):
     if len(my_list) >= idx < 0:
         return None
-    else:
-        return my_list[idx]
+
+    return my_list[idx]
