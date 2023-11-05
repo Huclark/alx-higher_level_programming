3ce98ed4563ab9d9baf94fdb7066a687561a12e4
Huclark <vhuclark@gmail.com>
2023-11-05 16:35:39 +0000

    task 5 correction

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -12,7 +12,7 @@ def no_c(my_string):
     new_str = ""
 
     for i in range(len(my_string)):
-        if my_string[i] not in "Cc":
+        if my_string[i] != 'c' and my_string[i] != 'C':
             new_str += my_string[i]
 
     return new_str
