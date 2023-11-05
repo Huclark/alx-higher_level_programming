f8eb38b12b20fa1de457780c9eac0d60e836ccaa
Huclark <vhuclark@gmail.com>
2023-11-05 16:39:14 +0000

    task 5 correction

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,15 +1,8 @@
 #!/usr/bin//python3
-
-"""
-no_c - Removes all characters c and C from a string.
-"""
-
-
+"""no_c - Removes all characters c and C from a string."""
 def no_c(my_string):
     new_str = ""
-
     for i in range(len(my_string)):
         if my_string[i] != 'c' and my_string[i] != 'C':
             new_str += my_string[i]
-
     return new_str
