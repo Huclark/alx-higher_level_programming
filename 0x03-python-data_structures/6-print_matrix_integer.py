d70aae1af8e87f388953f6d2261fb3b2d20ac02b
Huclark <vhuclark@gmail.com>
2023-11-05 20:25:34 +0000

    task 6 correction

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    """Prints a matrix of integers"""
-    for each_list in matrix:
-        new_list = ""
-        for item in each_list:
-            new_list += str(item) + " "
-        new_list = new_list.rstrip()
-        print("{}".format(new_list))
+    """ Prints a matrix of integers """
+    for idx1 in range(len(matrix)):
+        for idx2 in range(len(matrix[idx1])):
+            if idx2 != 0:
+                print(" ", end="")
+            print("{:d}".format(matrix[idx1][idx2]), end="")
+        print()
