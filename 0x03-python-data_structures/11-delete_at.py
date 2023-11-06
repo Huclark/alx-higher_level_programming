f58a956da68145fa598b95087bf1e07deb34b6e0
Huclark <vhuclark@gmail.com>
2023-11-06 11:39:04 +0000

    task 11

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    """Deletes the item at a specific position in a list
+    and returns the newly modified list
+    """
+    if idx >= len(my_list):
+        return my_list
+
+    del my_list[idx]
+
+    return my_list
