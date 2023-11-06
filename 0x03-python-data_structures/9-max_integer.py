74a80e9258ae3413988684c451f6e1ea34796d33
Huclark <vhuclark@gmail.com>
2023-11-06 11:12:31 +0000

    task 9 correction

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     """Finds amd returns the biggest number of a list"""
-    if list == []:
+    if len(my_list) == 0:
         return None
     biggest_num = my_list[0]
     for i in range(len(my_list)):
