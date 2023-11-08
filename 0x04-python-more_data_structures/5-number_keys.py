f6924d7b953ee75558bf2db04716f560289eb1a7
Huclark <vhuclark@gmail.com>
2023-11-08 13:20:36 +0000

    task 5

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def number_keys(a_dictionary):
+    """Returns the number of keys in a dictionary
+
+	a_dictionary is the dictionary passes to the function
+    """
+    no_of_keys = 0
+
+    for _ in a_dictionary:
+        no_of_keys += 1
+
+    return no_of_keys
