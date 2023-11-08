e915f2245c73a5f58fc9d9a9a4447fd26139f4fb
Huclark <vhuclark@gmail.com>
2023-11-08 11:07:47 +0000

    task 2

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    """Adds all unique integers in a list
+    (only once for each integer)
+    my_list is the list passed to the function
+    """
+    result = 0
+    unique_integers = set()
+    
+    for idx in range(len(my_list)):
+        if my_list[idx] not in unique_integers:
+            unique_integers.add(my_list[idx])
+            result += my_list[idx]
+
+    return result
