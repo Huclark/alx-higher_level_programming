1d9d22579c9a1580ee386fa2e8b72e93c681b5d8
Huclark <vhuclark@gmail.com>
2023-11-08 15:54:34 +0000

    task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    """ Converts a Roman numeral to an integer
+    assuming number will be between 1 to 3999.
+    """
+    if (roman_string is None) or (type(roman_string) is not str):
+        return 0
+
+    roman_numerals = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+
+    value = roman_numerals[roman_string[len(roman_string) - 1]]
+
+    for i in range(len(roman_string - 1), -1):
+        curr_value = roman_numerals[roman_string[i]]
+        prev_value = roman_numerals[roman_string[i - 1]]
+
+        if prev_value >= curr_value:
+            value += prev_value
+        else:
+            value -= prev_value
+
+    return value
