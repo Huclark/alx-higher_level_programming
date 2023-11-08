4ee98de743ab9ac75acfd6bfc46028c089635a31
Huclark <vhuclark@gmail.com>
2023-11-08 10:04:15 +0000

    "task 0"

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    """Computes and returns the square value
+    of all integers of a matrix
+    """
+    squared_matrix = []
+    
+    for matrix_idx in range(len(matrix)):
+        squared_list = []
+
+        for list_idx in range(len(matrix[matrix_idx])):
+            list_value = matrix[matrix_idx][list_idx]
+            squared_list.append(list_value ** 2)
+
+        squared_matrix.append(squared_list)
+    
+    return squared_matrix
