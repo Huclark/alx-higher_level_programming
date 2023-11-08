7a4842ba12e8eae78eb9fb0d237795c99b9eceb8
Huclark <vhuclark@gmail.com>
2023-11-08 14:56:33 +0000

    task 9 pycodestyle correction

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    """Returns a new dictionary with 
+    """Returns a new dictionary with
     all values multiplied by 2
     """
     new_dict = {}
