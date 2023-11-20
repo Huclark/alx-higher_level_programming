db7018736e27c5facd643ecb1b6f5c645f108003
Huclark <vhuclark@gmail.com>
2023-11-20 18:22:35 +0000

    Fix: Code bugs

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -15,7 +15,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[idx]), end="")
             count += 1
-        except (IndexError, ValueError):
+        except (TypeError, ValueError):
             pass
     print()
     return count
