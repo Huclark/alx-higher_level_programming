c0482f133f2bbbe634743d895335a57b9bced8c8
Huclark <vhuclark@gmail.com>
2023-11-20 21:11:01 +0000

    Add: Test file and solution

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def raise_exception_msg(message=""):
+    """Raises a name exception with a message
+
+    Args:
+        message: Message to raise
+    """
+    raise NameError(message)
