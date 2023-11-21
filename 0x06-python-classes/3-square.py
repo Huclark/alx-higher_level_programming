b2de6f5786d1b4cc061c76278db2ee427f478926
Huclark <vhuclark@gmail.com>
2023-11-21 18:38:50 +0000

    Add: Test and Solution files

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines a square
+"""
+class Square:
+    """Definition goes here
+    """
+    def __init__(self, size=0):
+        """Initializes a square instance
+
+        Args:
+            size (int, optional): The size of the square. Defaults to 0.
+        Note:
+            The size parameter is a private instance attribute
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
