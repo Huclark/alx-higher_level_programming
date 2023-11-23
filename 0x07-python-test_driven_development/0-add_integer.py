d26cf1fc3804fa8452b50ef6adef01b9c19e085a
Huclark <vhuclark@gmail.com>
2023-11-23 16:08:16 +0000

    Add: Code and test files

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Returns a + b
+"""
+
+
+def add_integer(a, b=98):
+    """This function adds two integers
+
+    Args:
+        a (int): 1st integer input
+        b (int, optional): 2nd integer input. Defaults to 98.
+    
+    Returns:
+        Addition of a and b
+    
+    Note:
+        Floats are typecast into integers
+    """
+    # Check if a is an integer or float
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+
+    # Check if b is an integer or float
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    # Typecast to int when returning addition
+    return int(a) + int(b)
