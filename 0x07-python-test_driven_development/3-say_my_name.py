cba7c1f9f73cfaa4bbd3452576453f46e5fa097a
Huclark <vhuclark@gmail.com>
2023-11-24 09:53:48 +0000

    Add: Function and test files

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Prints 'My name is <first name> <last name>'
+to stdout
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Prints 'My name is <first name> <last name> to
+    stdout
+
+    Args:
+        first_name (str): First name of user
+        last_name (str, optional): Last name of user. Defaults to "".
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not first_name:
+        raise ValueError("first_name can't be empty string")
+
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name.strip(), last_name.strip()))
+        
