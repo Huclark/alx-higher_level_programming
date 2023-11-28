a51872b750b6718af2bf9d8640578f798f7cd850
Huclark <vhuclark@gmail.com>
2023-11-28 15:06:59 +0000

    Fix: Boolean argument error

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -9,19 +9,19 @@ def add_integer(a, b=98):
     Args:
         a (int): 1st integer input
         b (int, optional): 2nd integer input. Defaults to 98.
-    
+
     Returns:
         Addition of a and b
-    
+
     Note:
         Floats are typecast into integers
     """
     # Check if a is an integer or float
-    if not isinstance(a, (int, float)):
+    if not isinstance(a, (int, float)) or isinstance(a, bool):
         raise TypeError("a must be an integer")
 
     # Check if b is an integer or float
-    if not isinstance(b, (int, float)):
+    if not isinstance(b, (int, float)) or isinstance(b, bool):
         raise TypeError("b must be an integer")
 
     # Typecast to int when returning addition
