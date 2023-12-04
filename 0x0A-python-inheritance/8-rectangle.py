2d68953bb7276d5737ce9c21b512203061db13e6
Huclark <vhuclark@gmail.com>
2023-12-04 19:24:44 +0000

    Fix: Blank line reuirement

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -6,6 +6,7 @@
 # Get the BaseGeometry class
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """A child class"""
     def __init__(self, width, height):
