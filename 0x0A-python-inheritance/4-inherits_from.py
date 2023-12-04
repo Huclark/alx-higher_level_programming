4524440be7f0d626a38ef3eb023290a683ebcd93
Huclark <vhuclark@gmail.com>
2023-12-04 17:55:14 +0000

    Add: Solution and test files

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A function that demonstrates class and inheritance"""
+
+
+def inherits_from(obj, a_class):
+    """Checks if an object is an instance of a class that 
+    inherited (directly or indirectly) from the specified class
+
+    Args:
+        obj: Object to check
+        a_class: Class to match
+
+    Returns:
+        bool: True if object is a match or False if otherwise
+    """
+    return type(obj) is not a_class and isinstance(obj, a_class)
