9f9e041cb17a300bf0d2132ceaad11be45d7a302
Huclark <vhuclark@gmail.com>
2023-12-05 17:38:43 +0000

    Add: Solution and test file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,5 +4,16 @@
 
 
 def write_file(filename="", text=""):
+    """Writes a string to a text file using encoding="utf-8"
+
+    Args:
+        filename (str, optional): Name of file. Defaults to "".
+        text (str, optional): Text to input. Defaults to "".
+
+    Returns:
+        int: Number of characters actually read and printed
+    """
+    if not filename:
+        return
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
