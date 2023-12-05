d135e7de8ec932a5f3e401b362a8f3d287ffdccb
Huclark <vhuclark@gmail.com>
2023-12-05 18:50:51 +0000

    fix: correcting solution

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -20,4 +20,4 @@ def append_write(filename="", text=""):
         with open(filename, "a", encoding="utf-8") as file:
             return file.write(text)
     except OSError as e:
-        return
+        return 0
