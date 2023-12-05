0146f79e02aef657c8dddb5e56ddd54145dc214a
Huclark <vhuclark@gmail.com>
2023-12-05 18:55:01 +0000

    Fix: final correction

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
-"""
-Contains the function "append_wrtie"
+"""A function that demonstrates file handling
 """
 
 
 def append_write(filename="", text=""):
-    """returns the number of chars appended to "filename" from "text" """
-    with open(filename, 'a', encoding='utf=8') as f:
-        return f.write(text)
+    """Appends a string at the end of a text file
+    using encoding=utf-8
+
+    Args:
+        filename (str, optional): Name of file. Defaults to "".
+        text (str, optional): Text to append to file. Defaults to "".
+
+    Returns:
+        int: Number of characters written to file
+    """
+    if not filename:
+        return 0 # return immediately if string is empty
+    with open(filename, "a", encoding="utf-8") as file:
+        return file.write(text)
