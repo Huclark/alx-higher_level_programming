b1270c464f6a3f893c64eb033f82414f86ab22e5
Huclark <vhuclark@gmail.com>
2024-01-16 23:49:19 +0000

    Add: Deleting data

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,7 @@
+-- Script that removes all records with a score <= 5 in the table second_table
+-- of the database hbtn_0c_0
+
+-- Select table to delete from
+DELETE FROM second_table
+-- Define criteria for deleting records
+WHERE score <= 5
