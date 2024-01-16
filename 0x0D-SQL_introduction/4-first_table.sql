b27287cf335ae9129470aac423de563ba1e16618
Huclark <vhuclark@gmail.com>
2024-01-16 16:48:04 +0000

    Add: creates the table, 'first_table'

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- Script that creates a table called 'first_table' in a given MySQL database
+
+-- Query to create the table if it doesn't exist
+CREATE TABLE IF NOT EXISTS first_table (
+	id INT,
+	name VARCHAR(256)
+);
