ca7289ca7c98b177395216d1da0a1d447753ff86
Huclark <vhuclark@gmail.com>
2024-01-17 18:21:45 +0000

    Fix: Database in which table should be created

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,12 +1,12 @@
 -- Script that creates the database 'hbtn_0d_usa' and the table 'states'
--- with the description;
+-- in the hbtn_0d_usa database with the description;
 -- id INT unique, auto generated, can’t be null and is a primary key
 -- name VARCHAR(256) can't be null
 
 -- Create database if it does not exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- Create table if it does not exist and populate it
-CREATE TABLE IF NOT EXISTS states(
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(256) NOT NULL
 );
