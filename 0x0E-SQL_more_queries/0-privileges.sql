e4932c7dd9d8a83895fc63e1440e3742a58263a3
Huclark <vhuclark@gmail.com>
2024-01-17 17:01:01 +0000

    Update: Comments

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,7 @@
 -- script that lists all privileges of the MySQL users;
 -- 'user_0d_1' and 'user_0d_2'
 
+-- List grants for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- List grants for user_0d_1
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
