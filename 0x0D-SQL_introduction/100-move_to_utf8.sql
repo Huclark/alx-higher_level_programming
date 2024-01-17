7f5dcc34fd58b8487c1a29c85865f5f18558a504
Huclark <vhuclark@gmail.com>
2024-01-17 02:29:24 +0000

    Add: checking checker requirements

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,14 +1,5 @@
--- Script that converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
--- Converts all of the following to UTF8;
--- Database hbtn_0c_0
--- Table first_table
--- Field name in first_table
+-- converts hbtn_0c-0 DB to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
+-- convert hbtn_0c_0, first_table, name 
 
--- Switch to the 'hbtn_0c_0' database
 USE `hbtn_0c_0`
--- Begin ALTERTABLE statement to modify the 'first_table' table
-ALTER TABLE `first_table`
--- Specify the new character set for the table as utf8mb4
-CONVERT TO CHARACTER SET utf8bm4
--- Specify the new collation for the table as 'utf8mb4_unicode_ci'
-COLLATE utf8mb4_unicode_ci;
+ALTER TABLE `first_table` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
