1861d2aeab02d27f4cb8e462096a51c93377cca4
Huclark <vhuclark@gmail.com>
2024-01-17 18:19:25 +0000

    Add: create hbtn_0d_usa database and states table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,12 @@
+-- Script that creates the database 'hbtn_0d_usa' and the table 'states'
+-- with the description;
+-- id INT unique, auto generated, can’t be null and is a primary key
+-- name VARCHAR(256) can't be null
+
+-- Create database if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- Create table if it does not exist and populate it
+CREATE TABLE IF NOT EXISTS states(
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(256) NOT NULL
+);
