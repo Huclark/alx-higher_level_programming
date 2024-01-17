87c67ef805b5e2127fca0783ae896e04092860af
Huclark <vhuclark@gmail.com>
2024-01-17 17:32:14 +0000

    Add: Grant select permission

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,8 @@
+-- Script that creates the database hbtn_0d_2 and the user 
+
+-- Create database if does not exist
+CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
+-- Create a user if it does not exist and set its password
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- Grant select permission to user.
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
