c42e20864d03728b11acef48d7fbd3321d0779f5
Huclark <vhuclark@gmail.com>
2024-01-17 17:35:25 +0000

    Fix: removed single quotes on line 4

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,7 @@
 -- Script that creates the database hbtn_0d_2 and the user 
 
 -- Create database if does not exist
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Create a user if it does not exist and set its password
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- Grant select permission to user.
