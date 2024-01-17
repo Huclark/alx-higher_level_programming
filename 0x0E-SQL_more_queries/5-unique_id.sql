a679b7345cfc62a04980f9c018a5c745f33dc30d
Huclark <vhuclark@gmail.com>
2024-01-17 18:04:56 +0000

    Add: Create table unique_id

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,9 @@
+-- Script that creates the table unique_id with the description;
+-- id INT with default value 1 and must be unique
+-- name VARCHAR(256)
+
+-- Create table if it does not exist
+CREATE TABLE IF NOT EXISTS unique_id(
+	id INT DEFAULT 1 UNIQUE,
+	name VARCHAR(256)
+);
