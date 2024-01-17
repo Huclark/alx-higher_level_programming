f739d65cd6fbb385b48dd53cd5247c49fb347f8d
Huclark <vhuclark@gmail.com>
2024-01-17 17:01:25 +0000

    Add: Grant all privileges

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,6 @@
+-- Script that creates the MySQL server user, 'user_d0_1'
+
+-- If user does not exist create one and set password
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- Grant all privileges to the user
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
