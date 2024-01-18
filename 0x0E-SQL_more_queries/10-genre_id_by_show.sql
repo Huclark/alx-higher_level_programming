ff7e7866fce5ac25b6f0dbe8f3c6af0e6e60d460
Huclark <vhuclark@gmail.com>
2024-01-18 03:40:13 +0000

    Fix: Commenting

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -4,7 +4,12 @@
 
 -- Select the column to retrieve data from
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows.title
+-- Select the table which the linking will be based on
+FROM tv_show_genres
+-- Use inner join to list all data
 INNER JOIN tv_shows
-ON tv_shows.id = tv_show_genres.id
+-- Specify the rule on which the tables should be joined
+ON tv_shows.id = tv_show_genres.show_id
+-- Order in ascending order by title and genre_id in
+-- tv_shows and tv_show_genres table, respectively
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
