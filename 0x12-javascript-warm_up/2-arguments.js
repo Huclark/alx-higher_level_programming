31233d2d87dee2e59c48831871202928122f848e
Huclark <vhuclark@gmail.com>
2024-02-12 16:00:47 +0000

    Add: task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+/**
+ * This script prints a message depending on the number of arguments
+ * passed.
+ * If no arguments are passed to the script, it prints "No argument"
+ * If only one argument is passed to the script, it prints "Argument found"
+ * Otherwise it prints "Arguments found"
+ */
+
+process.argv.length < 3
+  ? console.log('No argument')
+  : process.argv.length === 3
+    ? console.log('Argument found')
+    : console.log('Arguments found');
