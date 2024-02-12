e390dbdd08343bcadd4d7100c9efbf343f98de99
Huclark <vhuclark@gmail.com>
2024-02-12 22:42:39 +0000

    Fix: linting

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,7 +5,7 @@
  */
 const argvLength = process.argv.length;
 if (argvLength <= 3) {
-  console.log("0");
+  console.log('0');
 } else {
   let biggestNumber = parseInt(process.argv[2]);
   let secondBiggest = biggestNumber;
