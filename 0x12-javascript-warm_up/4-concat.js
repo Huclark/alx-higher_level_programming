5b42c0cfcedc459df12ed0c37ffea607e511bb0a
Huclark <vhuclark@gmail.com>
2024-02-12 16:37:51 +0000

    Add: Task 4

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+/**
+ * This script prints two arguments passed to it, in the following format:
+ * “ is ”
+ */
+const output = `${process.argv[2]} is ${process.argv[3]}`;
+
+console.log(output);
