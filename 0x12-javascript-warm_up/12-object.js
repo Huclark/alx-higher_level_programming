1db2d257c76432c04891147b13a642f7198a904f
Huclark <vhuclark@gmail.com>
2024-02-12 23:09:28 +0000

    Add: task 12

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+/**
+ * Updates the value 12 with 89
+ */
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
