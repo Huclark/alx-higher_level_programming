58e146e08a2087a86bf9f28a779f53fcc766f65e
Huclark <vhuclark@gmail.com>
2024-02-12 22:30:51 +0000

    Add: task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+/**
+ * This script searches for the second biggest integer in the list of arguments
+ * It prints 0 if zero or one argument is passed
+ */
+const argvLength = process.argv.length;
+if (argvLength <= 3) {
+  console.log(0);
+} else {
+  let biggestNumber = parseInt(process.argv[2]);
+  let secondBiggest = biggestNumber;
+  let currentNumber;
+  let index = 0;
+
+  while (index < argvLength) {
+    currentNumber = parseInt(process.argv[index]);
+    if (biggestNumber < currentNumber) {
+      secondBiggest = biggestNumber;
+      biggestNumber = currentNumber;
+    }
+    if (currentNumber > secondBiggest && currentNumber < biggestNumber) {
+      secondBiggest = currentNumber;
+    }
+    index++;
+  }
+  console.log(secondBiggest);
+}
