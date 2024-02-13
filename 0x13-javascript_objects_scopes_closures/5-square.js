caa0e29c59fa19e47bc1b638c90d772bdad5d66b
Huclark <vhuclark@gmail.com>
2024-02-13 14:27:56 +0000

    Add: Task 5

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/**
+ * This script defines a square and inherits from the Rectangle
+ * class of 4-rectangle.js file
+ * Constructor takes only one argument: size
+ */
+const Rectangle = require('./4-rectangle');
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+};
