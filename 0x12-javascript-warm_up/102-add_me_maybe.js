a6d4a2e6db3a793f2b6181515356face349c80b3
Huclark <vhuclark@gmail.com>
2024-02-13 00:17:30 +0000

    Fix: task 102

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -3,6 +3,6 @@
  * This function increments and calls a function
  */
 exports.addMeMaybe = function (number, theFunction) {
-  const newNumber = number + 1;
-  theFunction(newNumber);
+  ++number;
+  theFunction(number);
 };
