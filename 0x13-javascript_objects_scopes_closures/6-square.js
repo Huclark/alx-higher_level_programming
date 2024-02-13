e6a307cacd68914dd05c5b02810420139599808f
Huclark <vhuclark@gmail.com>
2024-02-13 15:05:07 +0000

    Fix: charprint()

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -12,7 +12,7 @@ module.exports = class Square extends Square1 {
       this.print();
     } else {
       for (let i = 0; i < this.height; i++) {
-        console.log('C'.repeat(this.height));
+        console.log(c.repeat(this.width));
       }
     }
   }
