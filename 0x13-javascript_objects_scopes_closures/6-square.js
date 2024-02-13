10a9786d137467fdf8434a91ba8efbc3796b6e2c
Huclark <vhuclark@gmail.com>
2024-02-13 14:56:42 +0000

    Add: Task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+/**
+ * This script defines a Square class
+ * and inherits from the Square class of 5-square.js file
+ * Constructor takes only one argument: size
+ */
+const Square1 = require('./5-square');
+module.exports = class Square extends Square1 {
+  // This prints the square
+  charPrint (c) {
+    if (!c) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log('C'.repeat(this.height));
+      }
+    }
+  }
+};
