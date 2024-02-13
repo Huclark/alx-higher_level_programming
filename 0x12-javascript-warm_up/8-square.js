09a20cab322e8ebbfc07670cfa8337269653ed2b
Huclark <vhuclark@gmail.com>
2024-02-13 00:23:08 +0000

    Fix: output fail

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,14 +4,20 @@
  * This script prints a square with the 'X' character
  * where the first argument is the size of the square
  */
-const size = parseInt(process.argv[2]);
-let height = 0;
+const process = require('process');
+const firstArg = process.argv[2];
+const size = parseInt(firstArg);
 
-if (size) {
-  while (height < size) {
-    console.log('X'.repeat(size));
-    height++;
+if (!isNaN(size)) {
+  if (size > 0) {
+    for (let i = 0; i < size; i++) {
+      let row = '';
+      for (let j = 0; j < size; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
   }
 } else {
-  console.log('Missing number of occurrences');
+  console.log('Missing size');
 }
