6c7a4f7bd5d2e26c4acd7373ee70651f6b61b5e8
Huclark <vhuclark@gmail.com>
2024-02-13 16:50:49 +0000

    Add: Task 11

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/**
+ * This script imports an array and computes a new array.
+ */
+const list = require('./100-main').list;
+console.log(list);
+const newList = list.map((number, index) => {
+  return number * index;
+});
+console.log(newList);
