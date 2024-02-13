969a56bf6dd037480ebba05be68838485b871043
Huclark <vhuclark@gmail.com>
2024-02-13 16:03:58 +0000

    Fix: camel case style

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,10 +3,10 @@
  * This function returns the reversed version of a list
  */
 exports.esrever = (list) => {
-  const new_list = [];
+  const newList = [];
   const len = list.length;
   for (let i = len - 1; i >= 0; i--) {
-    new_list.push(list[i]);
+    newList.push(list[i]);
   }
-  return new_list;
+  return newList;
 };
