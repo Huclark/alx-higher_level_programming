83fcad1f5a4c06e2b8901da3446babedb588b945
Huclark <vhuclark@gmail.com>
2024-02-15 20:39:01 +0000

    Add: Task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""This script displays all states with a name starting with
+N from the hbtn_0e_0_usa
+"""
+from sys import argv
+import MySQLdb
+
+
+def list_states(username, password, database, state_name):
+    """Lists all states which starts with letter N
+
+    Args:
+        username (str): Username
+        password (str): User's password
+        database (str): Database name
+        state_name (str): State name
+    """
+    db = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        password=password,
+        db=database,
+        port=3306
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name = %s", (state_name,))
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    if len(argv) != 5:
+        print("Usage: ./0-select_states.py <username> <password> <database>")
+        exit(1)
+    # execute function
+    list_states(argv[1], argv[2], argv[3], argv[4])
