b8bb957b0f4bf687ae049d0a490b1eeec80f66b2
Huclark <vhuclark@gmail.com>
2024-02-29 22:23:11 +0000

    Fix: make script 3 lines

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# This script takes in a URL, sends a request to that URL
-# and displays the size of the body of the response in bytes
-
+# Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
 curl -s -w "%{size_download}\n" -o /dev/null "$1"
