5a0f3822dac9ddf9d155a1357a620b3bdd3dd880
Huclark <vhuclark@gmail.com>
2024-03-02 21:51:02 +0000

    Add: task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This script takes in a URL, sends a request to the URL
+and displays the body of the response.
+"""
+from requests import get
+from sys import argv
+
+
+def print_body():
+    """Prints the body of the http response
+    """
+    response = get(argv[1])
+
+    if response.status_code >= 400:
+        print(f"Error code: {response.status_code}")
+    else:
+        print(response.text)
+
+
+if __name__ == "__main__":
+    print_body()
