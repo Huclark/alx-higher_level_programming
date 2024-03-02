7d06c19afd0ad7917b3e7205f7042408f7ff44a4
Huclark <vhuclark@gmail.com>
2024-03-02 20:11:47 +0000

    Fix: pep8

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""This script takes in a URL and an email, sends a POST request to the passed URL
-with the email as a parameter, and displays the body of the response (decoded in utf-8)
+"""This script takes in a URL and an email,
+sends a POST request to the passed URL
+with the email as a parameter,
+and displays the body of the response (decoded in utf-8)
 """
 from sys import argv
 from urllib.parse import urlencode
@@ -10,7 +12,7 @@ from urllib.request import urlopen
 def post_email():
     data = {}
     data['email'] = argv[2]
-    
+
     # Encode the data to be sent in the request body
     data_encoded = urlencode(data).encode('ascii')
 
@@ -22,4 +24,3 @@ def post_email():
 
 if __name__ == "__main__":
     post_email()
-    
