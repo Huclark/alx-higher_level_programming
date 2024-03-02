2c4f4addfe3281bb28559e218eaaf1a224968ad1
Huclark <vhuclark@gmail.com>
2024-03-02 17:14:58 +0000

    Fix: PEP8

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,9 +6,11 @@ of the response."""
 from urllib import request
 from sys import argv
 
+
 def x_request_id(args):
     with request.urlopen(args) as response:
         print(dict(response.headers).get('X-Request-Id'))
+
 
 if __name__ == "__main__":
     x_request_id(argv[1])
