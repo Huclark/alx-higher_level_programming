98c76a1dc4be99cc3478b198abfb03a1e1536fbc
Huclark <vhuclark@gmail.com>
2024-03-27 09:23:38 +0000

    Add: task 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/**
+ * This script displays the status code of a GET request
+ */
+const request = require('http');
+
+// Send a GET request to the provided URL
+request.get(url, (error, response) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
+});
---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+/**
+ * This script prints the title of a Star Wars movie where the episode
+ * number matches a given integer
+ */
